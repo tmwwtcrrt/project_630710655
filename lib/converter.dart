18ffc0f2986c08f9f71eb3d330597c7da3b7123f
Thamonwan Wutticharoenrat <nine_my15@hotmail.com>
2022-11-10 00:01:33 +0700

    ส่งโปรเจค

---
+++ lib/converter.dart
@@ -0,0 +1,49 @@
+class Converter {
+  static double THB2USD(double value){
+    return (value * 0.03);
+  }
+
+  static double USD2THB(double value){
+    return (value * 36.83);
+  }
+
+  static double THB2EUR(double value){
+    return (value * 0.03);
+  }
+
+  static double EUR2THB(double value){
+    return (value * 37.06);
+  }
+
+  static double THB2CNY(double value){
+    return (value * 0.20);
+  }
+
+  static double CNY2THB(double value){
+    return (value * 5.08);
+  }
+
+  static double THB2JPY(double value){
+    return (value * 3.95);
+  }
+
+  static double JPY2THB(double value){
+    return (value * 0.25);
+  }
+
+  static double THB2HKD(double value){
+    return (value * 0.21);
+  }
+
+  static double HKD2THB(double value){
+    return (value * 4.70);
+  }
+
+  static double THB2KRW(double value){
+    return (value * 37.07);
+  }
+
+  static double KRW2THB(double value){
+    return (value * 0.03);
+  }
+}
